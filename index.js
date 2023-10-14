a07151d8d8109d3c7feab2fe6267f1fbefc7eae8
pznamir00 <pznamir00@gmail.com>
2023-10-14 17:34:03 +0200

    refactor(index): add vars

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const showStonksGuy = function () {
+  const skinColor = "\x1b[38;2;255;239;184m";
+  const bgColor = "\x1b[30m";
+  const arrowColor = "\x1b[38;2;255;165;0m";
+  const suitColor = "\x1b[90m";
+  const shirtColor = "\x1b[34m";
+  const textColor = "\x1b[37m";
+  const textLayoutColor = "\x1b[35m";
+
   console.log(`
-                                                 \x1b[38;2;255;165;0m_*
-                \x1b[38;2;255;239;184m________\x1b[30m                      \x1b[38;2;255;165;0m_*   |
-              \x1b[38;2;255;239;184m/    /    \\\x1b[30m                 \x1b[38;2;255;165;0m_*       |
-             \x1b[38;2;255;239;184m/    / _   _\\\x1b[30m             \x1b[38;2;255;165;0m_*          |
-            \x1b[38;2;255;239;184m/    |  * | *|\x1b[30m           \x1b[38;2;255;165;0m*I            |
-           \x1b[38;2;255;239;184m|     |    \\  |\x1b[30m             \x1b[38;2;255;165;0m*_          |
-           \x1b[38;2;255;239;184m|     |    _| |\x1b[30m               \x1b[38;2;255;165;0m/      _  |
-           \x1b[38;2;255;239;184m|      \\ \\___//\x1b[30m              \x1b[38;2;255;165;0m/     /  *_|
-           \x1b[38;2;255;239;184m|   \\_  \\    /\x1b[30m              \x1b[38;2;255;165;0m/     /
-            \x1b[38;2;255;239;184m\\    \\__\\__/\x1b[30m              \x1b[38;2;255;165;0m/     /
-             \x1b[38;2;255;239;184m\\      /\x1b[30m                \x1b[38;2;255;165;0m/     /
-              \x1b[38;2;255;239;184m|    |\x1b[30m                \x1b[38;2;255;165;0m/     /
-        \x1b[90m_____/\\   \x1b[34m/\x1b[90m\\__\x1b[30m             \x1b[38;2;255;165;0m/     /
-      \x1b[90m/        \\ \x1b[34m/\x1b[90m  /  \\_____\x1b[30m     \x1b[38;2;255;165;0m/     /
-      \x1b[90m|         \\  /          \\\x1b[30m  \x1b[38;2;255;165;0m*_    /
-      \x1b[90m|          \\             \\\x1b[30m   \x1b[38;2;255;165;0m*_ /
-      \x1b[90m|       |   \\       |     \\\x1b[30m
-      \x1b[90m\\       |   \\      |       |\x1b[30m        \x1b[35m**************
-       \x1b[90m|       \\___|_____ |      |\x1b[30m        \x1b[35m*** \x1b[37mSTONKS \x1b[35m***
-        \x1b[90m\\                |""..    \\\x1b[30m       \x1b[35m**************
-        \x1b[90m|\\               |   _/    |\x1b[30m
-        \x1b[90m| \\______________|""       |\x1b[30m
-        \x1b[90m|           ""|_____      /
-        \x1b[90m                    """"""
+                                                 ${arrowColor}_*
+                ${skinColor}________${bgColor}                      ${arrowColor}_*   |
+              ${skinColor}/    /    \\${bgColor}                 ${arrowColor}_*       |
+             ${skinColor}/    / _   _\\${bgColor}             ${arrowColor}_*          |
+            ${skinColor}/    |  * | *|${bgColor}           ${arrowColor}*I            |
+           ${skinColor}|     |    \\  |${bgColor}             ${arrowColor}*_          |
+           ${skinColor}|     |    _| |${bgColor}               ${arrowColor}/      _  |
+           ${skinColor}|      \\ \\___//${bgColor}              ${arrowColor}/     /  *_|
+           ${skinColor}|   \\_  \\    /${bgColor}              ${arrowColor}/     /
+            ${skinColor}\\    \\__\\__/${bgColor}              ${arrowColor}/     /
+             ${skinColor}\\      /${bgColor}                ${arrowColor}/     /
+              ${skinColor}|    |${bgColor}                ${arrowColor}/     /
+        ${suitColor}_____/\\   ${shirtColor}/${suitColor}\\__${bgColor}             ${arrowColor}/     /
+      ${suitColor}/        \\ ${shirtColor}/${suitColor}  /  \\_____${bgColor}     ${arrowColor}/     /
+      ${suitColor}|         \\  /          \\${bgColor}  ${arrowColor}*_    /
+      ${suitColor}|          \\             \\${bgColor}   ${arrowColor}*_ /
+      ${suitColor}|       |   \\       |     \\${bgColor}
+      ${suitColor}\\       |   \\      |       |${bgColor}        ${textLayoutColor}**************
+       ${suitColor}|       \\___|_____ |      |${bgColor}        ${textLayoutColor}*** ${textColor}STONKS ${textLayoutColor}***
+        ${suitColor}\\                |""..    \\${bgColor}       ${textLayoutColor}**************
+        ${suitColor}|\\               |   _/    |${bgColor}
+        ${suitColor}| \\______________|""       |${bgColor}
+        ${suitColor}|           ""|_____      /
+        ${suitColor}                    """"""
         `);
 };
 
